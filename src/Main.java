40de7455d6b7b6ba4bd444f9278324d568b08d0c
Marcelo Rojas PC <mrojasl@pucp.edu.pe>
2022-06-02 21:25:44 -0500

    Probando conexión a database

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 package src;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args){
         try {
@@ -7,5 +11,28 @@ public class Main {
         } catch (ClassNotFoundException e){
             throw new RuntimeException(e);
         }
+        String user = "root";
+        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/mysystem4";
+
+        try {
+            Connection conn = DriverManager.getConnection(url,user,pass);
+            System.out.println("Conexión exitosa");
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from funciones");
+
+            resultSet.next();
+            int idSala = resultSet.getInt(1);
+            Date fecha = resultSet.getDate(2);
+            System.out.println("id: "+idSala);
+            System.out.println("fecha: "+fecha);
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
+
